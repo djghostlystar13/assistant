8c12df7525bfc7950f073d65adc839c30c6ae2e3
djghostlystar13 <34558890+djghostlystar13@users.noreply.github.com>
2017-12-29 23:08:53 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,25 +7,25 @@ client.on(  +'ready', () => {
 });
     
 client.on('message', message => {
-    if (message.content === prefix +'help') {
+    if (message.content = prefix +'help') {
     	message.reply('Hello im Dj_Ghostly-Star personal assistant, my commands are the following with out quotation marks , bot commands about the menu ! , " $status " , " $s " , " Download " , " $features " , " $gta5-key " ,  Other bot commands! " $help " , " $ping " , " $bing " , " no " , " yes " ," hi " , " hello " , " hi " ," Bye " , " Goodbye " , " Goodnight " , " night ". ');
   	}
 
 //menu stuff 
 
-    if (message.content === 'download') {
+    if (message.content = 'download') {
     	message.reply('the cheat is currently not available, please be patient as there is only one developer.');
   	}
-          if (message.content === 'Download') {
+          if (message.content = 'Download') {
             message.reply('the cheat is currently not available, please be patient as there is only one developer.');
         }
 
 
 
-    if (message.content === prefix +'status') {
+    if (message.content = prefix +'status') {
     	message.reply('BOT = online, menu = Offline/detected.');
   	}
-        if (message.content === prefix +'s') {
+        if (message.content = prefix +'s') {
             message.reply('BOT = online, menu = Offline/detected.');
         }
     
@@ -39,71 +39,71 @@ client.on('message', message => {
 });
    */
 
-    if (message.content === prefix +'features') {
+    if (message.content = prefix +'features') {
     	message.reply('List of features in the menu! Godmode , DemiGod , *Teleport , No Reload , Infinite Ammo , Bullet Damage , RP increase , Never wanted , Super jump , Fast run ,Vehicle god , No recoil , Set time . *means this feature is buggy and sometimes crashes game . ');
   	}
 
 
 //fun featues of bot
 
-    if (message.content === 'No') {
+    if (message.content = 'No') {
     	message.reply('Yes!');
   	}
-            if (message.content === 'no') {
+            if (message.content = 'no') {
     	        message.reply('Yes!');
   	       }
-     if (message.content === 'Yes') {
+     if (message.content = 'Yes') {
     	message.reply('No!');
   	}
-            if (message.content === 'yes') {
+            if (message.content = 'yes') {
     	        message.reply('No!');
   	      }
 
 
 
-     if (message.content === 'hi') {
+     if (message.content = 'hi') {
     	message.reply('Bye!');
   	}
-            if (message.content === 'Hi') {
+            if (message.content = 'Hi') {
     	        message.reply('Bye!');
   	        }
-    if (message.content === 'hello') {
+    if (message.content = 'hello') {
     	message.reply('Goodbye!');
   	}
-            if (message.content === 'Hello') {
+            if (message.content = 'Hello') {
     	        message.reply('Goodbye!');
   	        }
-     if (message.content === 'bye') {
+     if (message.content = 'bye') {
     	message.reply('Hello!');
   	}
-    if (message.content === 'goodbye') {
+    if (message.content = 'goodbye') {
     	message.reply('Hello!');
   	}
-            if (message.content === 'Goodbye') {
+            if (message.content = 'Goodbye') {
     	          message.reply('Hello!');
   	        }
 
 
 
-    if (message.content === prefix +'ping') {
+    if (message.content = prefix +'ping') {
     	message.channel.send('PONG!');
   	}
-       if (message.content === prefix +'bing') {
+       if (message.content = prefix +'bing') {
     	message.reply('BONG!');
   	}
 
 
    
-    if (message.content === 'goodnight') {
+    if (message.content = 'goodnight') {
     	message.reply('Goodnight, sleep well, talk to you again soon');
   	}
-           if (message.content === 'Goodnight') {
+           if (message.content = 'Goodnight') {
             message.reply('Goodnight, sleep well, talk to you again soon');
             }
-                 if (message.content === 'night') {
+                 if (message.content = 'night') {
                     message.reply('Goodnight, sleep well, talk to you again soon');
                     }
-                        if (message.content === 'Night') {
+                        if (message.content = 'Night') {
                     message.reply('Goodnight, sleep well, talk to you again soon');
                     }
     });
