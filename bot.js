1f3b70509a84c0c0d33b19f66850b2ef9e434e85
djghostlystar13 <34558890+djghostlystar13@users.noreply.github.com>
2017-12-29 23:07:13 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,20 +8,20 @@ client.on(  +'ready', () => {
     
 client.on('message', message => {
     if (message.content === prefix +'help') {
-    	message.reply('Hello im Ghostly-2.0, my commands are the following with out quotation marks , bot commands about the menu ! , " $status " , " $s " , " Download " , " $features " , " $gta5-key " ,  Other bot commands! " $help " , " $ping " , " $bing " , " no " , " yes " ," hi " , " hello " , " hi " ," Bye " , " Goodbye " , " Goodnight " , " night ". ');
+    	message.reply('Hello im Dj_Ghostly-Star personal assistant, my commands are the following with out quotation marks , bot commands about the menu ! , " $status " , " $s " , " Download " , " $features " , " $gta5-key " ,  Other bot commands! " $help " , " $ping " , " $bing " , " no " , " yes " ," hi " , " hello " , " hi " ," Bye " , " Goodbye " , " Goodnight " , " night ". ');
   	}
-});
+
 //menu stuff 
-client.on('message', message => {
+
     if (message.content === 'download') {
     	message.reply('the cheat is currently not available, please be patient as there is only one developer.');
   	}
           if (message.content === 'Download') {
             message.reply('the cheat is currently not available, please be patient as there is only one developer.');
         }
-});
 
-client.on('message', message => {
+
+
     if (message.content === prefix +'status') {
     	message.reply('BOT = online, menu = Offline/detected.');
   	}
@@ -29,23 +29,23 @@ client.on('message', message => {
             message.reply('BOT = online, menu = Offline/detected.');
         }
     
-});
+
 /*
-    client.on('message', message => {
+  
     if (message.content === prefix +'gta5-key') {
     	 message.channel.send("Please check your **DM**s");
         message.author.send('');
   	}
 });
    */
-client.on('message', message => {
+
     if (message.content === prefix +'features') {
     	message.reply('List of features in the menu! Godmode , DemiGod , *Teleport , No Reload , Infinite Ammo , Bullet Damage , RP increase , Never wanted , Super jump , Fast run ,Vehicle god , No recoil , Set time . *means this feature is buggy and sometimes crashes game . ');
   	}
-});
+
 
 //fun featues of bot
-client.on('message', message => {
+
     if (message.content === 'No') {
     	message.reply('Yes!');
   	}
@@ -58,9 +58,9 @@ client.on('message', message => {
             if (message.content === 'yes') {
     	        message.reply('No!');
   	      }
-});
 
-client.on('message', message => {
+
+
      if (message.content === 'hi') {
     	message.reply('Bye!');
   	}
@@ -82,18 +82,18 @@ client.on('message', message => {
             if (message.content === 'Goodbye') {
     	          message.reply('Hello!');
   	        }
-});
 
-client.on('message', message => {
+
+
     if (message.content === prefix +'ping') {
     	message.channel.send('PONG!');
   	}
        if (message.content === prefix +'bing') {
     	message.reply('BONG!');
   	}
-});
 
-   client.on('message', message => {
+
+   
     if (message.content === 'goodnight') {
     	message.reply('Goodnight, sleep well, talk to you again soon');
   	}
